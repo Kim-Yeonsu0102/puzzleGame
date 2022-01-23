5cf1d3d2d7edb021742ae74f1a35337582bc27fb
Kim yeon su <rla5258@gmail.com>
2022-01-23 22:04:12 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let startBtn = document.querySelector(".start-btn")
+let playTime = document.querySelector(".play-time")
+
+
+startBtn.addEventListener("click", start)
+
+function start() {
+    console.log("start")
+}
